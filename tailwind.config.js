e3046a99bf9a590adaa3719e8f3d3c1f6da28675
thejunghare <prasadjunghare2@gmail.com>
2024-04-16 21:45:12 +0530

    updated dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 module.exports = {
-  content: ["./App.{js,jsx,ts,tsx}",,"./components/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: ["./App.{js,jsx,ts,tsx}", , "./components/**/*.{js,jsx,ts,tsx}"],
+    theme: {
+        extend: {
+            colors: {
+                'ezgo-red': '#C51E3A',
+            },
+        },
+    },
+    plugins: [],
 }
 
