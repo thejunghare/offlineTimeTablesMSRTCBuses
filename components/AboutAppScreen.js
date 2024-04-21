6d3ca855a896b80a9c9c6214c068175fcbe60d7f
thejunghare <prasadjunghare2@gmail.com>
2024-04-21 21:55:22 +0530

    updated ui for about app

--- components/AboutAppScreen.js
+++ components/AboutAppScreen.js
@@ -1,10 +1,55 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View } from "react-native";
+import { Text, List, TouchableRipple, RadioButton } from "react-native-paper";
 
 const AboutAppScreen = () => {
+  const [expanded, setExpanded] = React.useState(true);
+
+  const handlePress = () => setExpanded(!expanded);
+
   return (
     <View className="flex-1 w-full bg-gray-50">
-      <Text className="text-base text-center mt-5">About app</Text>
+      <Text className="text-xs font-bold px-5 mt-5 ">App information</Text>
+
+      <View className="bg-white border border-slate-200 m-5 rounded-xl">
+        <List.Item
+          title="Version type"
+          className="border-b border-slate-200"
+          left={(props) => (
+            <List.Icon {...props} icon="application-edit-outline" />
+          )}
+          right={(props) => (
+            <Text className="py-2 font-bold font-base pr-2 text-ezgo-red">
+              Beta
+            </Text>
+          )}
+        />
+
+        <List.Item
+          title="App version"
+          className="border-b border-slate-200"
+          left={(props) => (
+            <List.Icon {...props} icon="application-brackets-outline" />
+          )}
+          right={(props) => (
+            <Text className="py-2 font-bold font-base pr-2 text-ezgo-red">
+              v0.0.6.8
+            </Text>
+          )}
+        />
+
+        <List.Item
+          title="Version name"
+          left={(props) => (
+            <List.Icon {...props} icon="application-edit-outline" />
+          )}
+          right={(props) => (
+            <Text className="py-2 font-bold font-base pr-2 text-ezgo-red">
+              Early bird
+            </Text>
+          )}
+        />
+      </View>
     </View>
   );
 };
