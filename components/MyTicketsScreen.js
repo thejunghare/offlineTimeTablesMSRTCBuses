00c7037d39f2597291dde23b6dd32c68e51ce78c
thejunghare <prasadjunghare2@gmail.com>
2024-04-25 00:11:41 +0530

    my ticket screen

---
+++ components/MyTicketsScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {ScrollView, View, Text} from 'react-native'
+
+const myTicketsScreen = () => {
+    return(
+        <ScrollView className={"flex-1 bg-gray-50"}>
+            <Text>My tickets screen</Text>
+        </ScrollView>
+    )
+}
+
+export default myTicketScreen()
