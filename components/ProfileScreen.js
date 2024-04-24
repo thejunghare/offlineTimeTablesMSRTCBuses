9661c88aad0d6d00fbaf373df2099b8aaffd5b6b
thejunghare <prasadjunghare2@gmail.com>
2024-04-25 00:14:57 +0530

    add function to access my tickets

--- components/ProfileScreen.js
+++ components/ProfileScreen.js
@@ -1,130 +1,135 @@
-import React, { useEffect, useState } from "react";
-import { ScrollView,Text, View } from "react-native";
-import { firebase, auth } from "../firebase";
-import { useNavigation } from "@react-navigation/native";
-import { List, TouchableRipple } from "react-native-paper";
+import React, {useEffect, useState} from "react";
+import {ScrollView, Text, View} from "react-native";
+import {firebase, auth} from "../firebase";
+import {useNavigation} from "@react-navigation/native";
+import {List, TouchableRipple} from "react-native-paper";
 
 const ProfileScreen = () => {
-  const navigation = useNavigation();
+    const navigation = useNavigation();
 
-  const handleAccountPress = () => {
-    navigation.navigate("AccountScreen");
-  };
-
-  const handleHelpPress = () => {
-    navigation.navigate("HelpScreen");
-  };
-
-  const handleSupportPress = () => {
-    navigation.navigate("CallSupportScreen");
-  };
-
-  const handleFeedbackPress = () => {
-    navigation.navigate("FeedbackScreen");
-  };
-
-  const handleLogout = () => {
-    auth
-      .signOut()
-      .then(() => {
-        navigation.replace("Login");
-      })
-      .catch((error) => alert(error.message));
-  };
-
-  const [currentUser, setCurrentUser] = useState(null);
-  const [currentUserTicket, setCurrentUserTicket] = useState(null);
-  const [currentUserMonthlyPass, setCurrentUserMonthlyPass] = useState(null);
-
-  useEffect(() => {
-    const fetchUserDetails = async () => {
-      // Get the currently authenticated user
-      const user = firebase.auth().currentUser;
-      if (user) {
-        // Get the user document from Firestore using the user's UID
-        const userRef = firebase.firestore().collection("users").doc(user.uid);
-
-        try {
-          const docSnapshot = await userRef.get();
-          if (docSnapshot.exists) {
-            // Set the user data to state
-            setCurrentUser(docSnapshot.data());
-          } else {
-            console.error("User document does not exist");
-          }
-        } catch (error) {
-          console.error("Error fetching user: ", error);
-        }
-      } else {
-        console.error("No user is currently authenticated");
-      }
+    const handleAccountPress = () => {
+        navigation.navigate("AccountScreen");
     };
 
-    const fetchUserMonthlyPass = async () => {
-      try {
-        const currentUser = firebase.auth().currentUser;
-        const monthlyPassRef = firebase
-          .firestore()
-          .collection("users")
-          .doc(currentUser.uid)
-          .collection("monthlyPass");
+    const handleMyTickets = () => {
+        navigation.navigate("MyTicketsScreen")
+    }
 
-        const querySnapshot = await monthlyPassRef.get();
-
-        const userPasses = [];
-        querySnapshot.forEach((doc) => {
-          userPasses.push({
-            id: doc.id,
-            data: doc.data(),
-          });
-        });
-
-        setCurrentUserMonthlyPass(userPasses);
-      } catch (error) {
-        console.error("Error fetching user monthly passes: ", error);
-      }
+    const handleHelpPress = () => {
+        navigation.navigate("HelpScreen");
     };
 
-    const fetchUserTickets = async () => {
-      try {
-        const currentUser = firebase.auth().currentUser;
-        const unreversedTicketsRef = firebase
-          .firestore()
-          .collection("users")
-          .doc(currentUser.uid)
-          .collection("unreversedTickets");
-
-        const querySnapshot = await unreversedTicketsRef.get();
-
-        const userTickets = [];
-        querySnapshot.forEach((doc) => {
-          userTickets.push({
-            id: doc.id,
-            data: doc.data(),
-          });
-        });
-
-        setCurrentUserTicket(userTickets);
-      } catch (error) {
-        console.error("Error fetching user tickets: ", error);
-      }
+    const handleSupportPress = () => {
+        navigation.navigate("CallSupportScreen");
     };
 
-    fetchUserDetails();
-    fetchUserTickets();
-    fetchUserMonthlyPass();
+    const handleFeedbackPress = () => {
+        navigation.navigate("FeedbackScreen");
+    };
 
-    return () => {};
-  }, []);
+    const handleLogout = () => {
+        auth
+            .signOut()
+            .then(() => {
+                navigation.replace("Login");
+            })
+            .catch((error) => alert(error.message));
+    };
 
-  return (
-    <ScrollView className="flex-1 w-full bg-gray-50">
-      <Text className="text-xs font-bold px-5 mt-5 ">Account</Text>
+    const [currentUser, setCurrentUser] = useState(null);
+    const [currentUserTicket, setCurrentUserTicket] = useState(null);
+    const [currentUserMonthlyPass, setCurrentUserMonthlyPass] = useState(null);
 
-      {/* {currentUser && <Text>{currentUser.userPhoneNumber}</Text>} */}
+    useEffect(() => {
+        const fetchUserDetails = async () => {
+            // Get the currently authenticated user
+            const user = firebase.auth().currentUser;
+            if (user) {
+                // Get the user document from Firestore using the user's UID
+                const userRef = firebase.firestore().collection("users").doc(user.uid);
 
-      {/* access unreversed tickets */}
-      {/*    {currentUserTicket !== null && currentUserTicket.length > 0 ? (
+                try {
+                    const docSnapshot = await userRef.get();
+                    if (docSnapshot.exists) {
+                        // Set the user data to state
+                        setCurrentUser(docSnapshot.data());
+                    } else {
+                        console.error("User document does not exist");
+                    }
+                } catch (error) {
+                    console.error("Error fetching user: ", error);
+                }
+            } else {
+                console.error("No user is currently authenticated");
+            }
+        };
+
+        const fetchUserMonthlyPass = async () => {
+            try {
+                const currentUser = firebase.auth().currentUser;
+                const monthlyPassRef = firebase
+                    .firestore()
+                    .collection("users")
+                    .doc(currentUser.uid)
+                    .collection("monthlyPass");
+
+                const querySnapshot = await monthlyPassRef.get();
+
+                const userPasses = [];
+                querySnapshot.forEach((doc) => {
+                    userPasses.push({
+                        id: doc.id,
+                        data: doc.data(),
+                    });
+                });
+
+                setCurrentUserMonthlyPass(userPasses);
+            } catch (error) {
+                console.error("Error fetching user monthly passes: ", error);
+            }
+        };
+
+        const fetchUserTickets = async () => {
+            try {
+                const currentUser = firebase.auth().currentUser;
+                const unreversedTicketsRef = firebase
+                    .firestore()
+                    .collection("users")
+                    .doc(currentUser.uid)
+                    .collection("unreversedTickets");
+
+                const querySnapshot = await unreversedTicketsRef.get();
+
+                const userTickets = [];
+                querySnapshot.forEach((doc) => {
+                    userTickets.push({
+                        id: doc.id,
+                        data: doc.data(),
+                    });
+                });
+
+                setCurrentUserTicket(userTickets);
+            } catch (error) {
+                console.error("Error fetching user tickets: ", error);
+            }
+        };
+
+        fetchUserDetails();
+        fetchUserTickets();
+        fetchUserMonthlyPass();
+
+        return () => {
+        };
+    }, []);
+
+    return (
+        <ScrollView className="flex-1 w-full bg-gray-50">
+            <Text className="text-xs font-bold px-5 mt-5 ">Account</Text>
+
+            {/* {currentUser && <Text>{currentUser.userPhoneNumber}</Text>} */}
+
+            {/* access unreversed tickets */}
+            {/*    {currentUserTicket !== null && currentUserTicket.length > 0 ? (
         currentUserTicket.map((ticket, index) => (
           <View key={index} style={{ marginBottom: 10 }}>
             <Text>Ticket ID: {ticket.id}</Text>
@@ -137,8 +142,8 @@ const ProfileScreen = () => {
         <Text>No tickets found.</Text>
       )} */}
 
-      {/* access monthly passes */}
-      {/* {currentUserMonthlyPass !== null && currentUserMonthlyPass.length > 0 ? (
+            {/* access monthly passes */}
+            {/* {currentUserMonthlyPass !== null && currentUserMonthlyPass.length > 0 ? (
         currentUserMonthlyPass.map((pass, index) => (
           <View key={index} style={{ marginBottom: 10 }}>
             <Text>Ticket ID: {pass.id}</Text>
@@ -151,107 +156,108 @@ const ProfileScreen = () => {
         <Text>No pass found.</Text>
       )} */}
 
-      <View className="bg-white border border-slate-200 m-5 rounded-xl">
-        <TouchableRipple
-          rippleColor="rgba(0, 0, 0, .32)"
-          className="border-b border-slate-200"
-          onPress={handleLogout}
-        >
-          <List.Item
-            title={currentUser && currentUser.displayName}
-            description={auth.currentUser?.email}
-            left={(props) => <List.Icon {...props} icon="account-outline" />}
-            right={(props) => <List.Icon {...props} icon="logout-variant" />}
-          />
-        </TouchableRipple>
+            <View className="bg-white border border-slate-200 m-5 rounded-xl">
+                <TouchableRipple
+                    rippleColor="rgba(0, 0, 0, .32)"
+                    className="border-b border-slate-200"
+                    onPress={handleLogout}
+                >
+                    <List.Item
+                        title={currentUser && currentUser.displayName}
+                        description={auth.currentUser?.email}
+                        left={(props) => <List.Icon {...props} icon="account-outline"/>}
+                        right={(props) => <List.Icon {...props} icon="logout-variant"/>}
+                    />
+                </TouchableRipple>
 
-        {/* update account details */}
-        <TouchableRipple
-          rippleColor="rgba(0, 0, 0, .32)"
-          className=""
-          onPress={handleAccountPress}
-        >
-          <List.Item
-            title="Settings"
-            description="Update profile, update password"
-            left={(props) => (
-              <List.Icon {...props} icon="account-settings-outline" />
-            )}
-            right={(props) => <List.Icon {...props} icon="chevron-right" />}
-          />
-        </TouchableRipple>
-      </View>
+                {/* update account details */}
+                <TouchableRipple
+                    rippleColor="rgba(0, 0, 0, .32)"
+                    className=""
+                    onPress={handleAccountPress}
+                >
+                    <List.Item
+                        title="Settings"
+                        description="Update profile, update password"
+                        left={(props) => (
+                            <List.Icon {...props} icon="account-settings-outline"/>
+                        )}
+                        right={(props) => <List.Icon {...props} icon="chevron-right"/>}
+                    />
+                </TouchableRipple>
+            </View>
 
-      <Text className="text-xs font-bold px-5">My bookings</Text>
-      <View className="bg-white border border-slate-200 m-5 rounded-xl">
-        <TouchableRipple
-          rippleColor="rgba(0, 0, 0, .32)"
-          className="border-b border-slate-200"
-        >
-          <List.Item
-            title="Tickets"
-            description="Unversed tickets"
-            left={(props) => (
-              <List.Icon {...props} icon="ticket-confirmation-outline" />
-            )}
-            right={(props) => <List.Icon {...props} icon="chevron-right" />}
-          />
-        </TouchableRipple>
+            <Text className="text-xs font-bold px-5">My bookings</Text>
+            <View className="bg-white border border-slate-200 m-5 rounded-xl">
+                <TouchableRipple
+                    rippleColor="rgba(0, 0, 0, .32)"
+                    className="border-b border-slate-200"
+onPress={handleMyTickets}
+                >
+                    <List.Item
+                        title="Tickets"
+                        description="Unversed tickets"
+                        left={(props) => (
+                            <List.Icon {...props} icon="ticket-confirmation-outline"/>
+                        )}
+                        right={(props) => <List.Icon {...props} icon="chevron-right"/>}
+                    />
+                </TouchableRipple>
 
-        <TouchableRipple rippleColor="rgba(0, 0, 0, .32)">
-          <List.Item
-            title="Monthly Pass"
-            description="Monthly pass"
-            left={(props) => <List.Icon {...props} icon="ticket-account" />}
-            right={(props) => <List.Icon {...props} icon="chevron-right" />}
-          />
-        </TouchableRipple>
-      </View>
+                <TouchableRipple rippleColor="rgba(0, 0, 0, .32)">
+                    <List.Item
+                        title="Monthly Pass"
+                        description="Monthly pass"
+                        left={(props) => <List.Icon {...props} icon="ticket-account"/>}
+                        right={(props) => <List.Icon {...props} icon="chevron-right"/>}
+                    />
+                </TouchableRipple>
+            </View>
 
-      <Text className="text-xs font-bold px-5">Support</Text>
-      <View className="bg-white border border-slate-200 m-5 rounded-xl">
-        <TouchableRipple
-          rippleColor="rgba(0, 0, 0, .32)"
-          className="border-b border-slate-200"
-          onPress={handleHelpPress}
-        >
-          <List.Item
-            title="Help"
-            description="Payments, tickets, privacy policy"
-            left={(props) => (
-              <List.Icon {...props} icon="help-circle-outline" />
-            )}
-            right={(props) => <List.Icon {...props} icon="chevron-right" />}
-          />
-        </TouchableRipple>
+            <Text className="text-xs font-bold px-5">Support</Text>
+            <View className="bg-white border border-slate-200 m-5 rounded-xl">
+                <TouchableRipple
+                    rippleColor="rgba(0, 0, 0, .32)"
+                    className="border-b border-slate-200"
+                    onPress={handleHelpPress}
+                >
+                    <List.Item
+                        title="Help"
+                        description="Payments, tickets, privacy policy"
+                        left={(props) => (
+                            <List.Icon {...props} icon="help-circle-outline"/>
+                        )}
+                        right={(props) => <List.Icon {...props} icon="chevron-right"/>}
+                    />
+                </TouchableRipple>
 
-        <TouchableRipple
-          rippleColor="rgba(0, 0, 0, .32)"
-          className="border-b border-slate-200"
-          onPress={handleSupportPress}
-        >
-          <List.Item
-            title="Support"
-            description="Contact us, write us"
-            left={(props) => <List.Icon {...props} icon="call-made" />}
-            right={(props) => <List.Icon {...props} icon="chevron-right" />}
-          />
-        </TouchableRipple>
+                <TouchableRipple
+                    rippleColor="rgba(0, 0, 0, .32)"
+                    className="border-b border-slate-200"
+                    onPress={handleSupportPress}
+                >
+                    <List.Item
+                        title="Support"
+                        description="Contact us, write us"
+                        left={(props) => <List.Icon {...props} icon="call-made"/>}
+                        right={(props) => <List.Icon {...props} icon="chevron-right"/>}
+                    />
+                </TouchableRipple>
 
-        <TouchableRipple
-          rippleColor="rgba(0, 0, 0, .32)"
-          onPress={handleFeedbackPress}
-        >
-          <List.Item
-            title="Feedback"
-            description="Bus feedback, tickets feedback"
-            left={(props) => <List.Icon {...props} icon="bug-outline" />}
-            right={(props) => <List.Icon {...props} icon="chevron-right" />}
-          />
-        </TouchableRipple>
-      </View>
-    </ScrollView>
-  );
+                <TouchableRipple
+                    rippleColor="rgba(0, 0, 0, .32)"
+                    onPress={handleFeedbackPress}
+                >
+                    <List.Item
+                        title="Feedback"
+                        description="Bus feedback, tickets feedback"
+                        left={(props) => <List.Icon {...props} icon="bug-outline"/>}
+                        right={(props) => <List.Icon {...props} icon="chevron-right"/>}
+                    />
+                </TouchableRipple>
+            </View>
+        </ScrollView>
+    );
 };
 
 export default ProfileScreen;
--- components/ProfileScreenStack.js
+++ components/ProfileScreenStack.js
@@ -1,69 +1,93 @@
 import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-
+import {NavigationContainer} from "@react-navigation/native";
+import {createStackNavigator} from "@react-navigation/stack";
 import AccountScreen from "./AccountScreen";
 import HelpScreen from "./HelpScreen";
 import CallSupportScreen from "./CallSupportScreen";
 import FeedbackScreen from "./FeedbackScreen";
+import MyTicketsScreen from "./MyTicketsScreen";
+import Signup from "./Signup";
 
 const Stack = createStackNavigator();
 
 const ScreenNames = {
-  ACCOUNT: "AccountScreen",
-  HELP: "HelpScreen",
-  CALLSUPPORT: "CallSupportScreen",
-  FEEDBACK: "FeedbackScreen",
+    AccountScreen: "AccountScreen",
+    MyTicketsScreen: "MyTicketsScreen"
+    Help: "HelpScreen",
+    CallSupportScreen: "CallSupportScreen",
+    Feedback: "FeedbackScreen",
+    Signup: "Signup",
 };
 
+const isLoggedIn = true;
+
 const StackNavigator = () => {
-  return (
-    <Stack.Navigator
-      screenOptions={{
-        headerMode: "screen",
-        headerStyle: {
-          backgroundColor: "white",
-        },
-      }}
-    >
-      <Stack.Screen
-        name={ScreenNames.ACCOUNT}
-        component={AccountScreen}
-        options={{
-          title: "Account",
-        }}
-      />
-      <Stack.Screen
-        name={ScreenNames.HELP}
-        component={HelpScreen}
-        options={{
-          title: "Help",
-        }}
-      />
-      <Stack.Screen
-        name={ScreenNames.CALLSUPPORT}
-        component={CallSupportScreen}
-        options={{
-          title: "Call support",
-        }}
-      />
-      <Stack.Screen
-        name={ScreenNames.FEEDBACK}
-        component={FeedbackScreen}
-        options={{
-          title: "Feedback",
-        }}
-      />
-    </Stack.Navigator>
-  );
+    return (
+        <Stack.Navigator
+            screenOptions={{
+                headerMode: "screen",
+                headerStyle: {
+                    backgroundColor: "white",
+                },
+            }}
+        >
+            {isLoggedIn ? (
+                <>
+                    <Stack.Screen
+                        name={ScreenNames.AccountScreen}
+                        component={AccountScreen}
+                        options={{
+                            title: "Account details",
+                        }}
+                    />
+                <Stack.Screen
+                                        name={ScreenNames.MyTicketsScreen}
+                                        component={MyTicketsScreen}
+                                        options={{
+                                            title: "My tickets",
+                                        }}
+                                    />
+                    <Stack.Screen
+                        name={ScreenNames.Feedback}
+                        component={FeedbackScreen}
+                        options={{
+                            title: "Feedback",
+                        }}
+                    />
+                </>
+            ) : (
+                <Stack.Screen
+                    name={ScreenNames.Signup}
+                    component={Signup}
+                    options={{
+                        title: "Signup",
+                    }}
+                />
+            )}
+            <Stack.Screen
+                name={ScreenNames.Help}
+                component={HelpScreen}
+                options={{
+                    title: "Help",
+                }}
+            />
+            <Stack.Screen
+                name={ScreenNames.CallSupportScreen}
+                component={CallSupportScreen}
+                options={{
+                    title: "Call support",
+                }}
+            />
+        </Stack.Navigator>
+    );
 };
 
 const ProfileScreenStack = () => {
-  return (
-    <NavigationContainer independent={true}>
-      <StackNavigator />
-    </NavigationContainer>
-  );
+    return (
+        <NavigationContainer>
+            <StackNavigator/>
+        </NavigationContainer>
+    );
 };
 
 export default ProfileScreenStack;
